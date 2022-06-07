04ea48d05414e5e6ca2537b9884234c3cdc6c7ec
Tim Haak <tim@haak.co>
2022-06-07 21:13:52 +0200

    New build

--- baseBuildNew.sh
+++ baseBuildNew.sh
@@ -12,13 +12,13 @@ echo "Tagged as : ${IMAGE_NAME}"
 echo ""
 echo ""
 
-docker context create blue --default-stack-orchestrator=swarm --docker "host=${DOCKER_HOST},ca=${DOCKER_CERT_PATH}/ca.pem,cert=${DOCKER_CERT_PATH}/cert.pem,key=${DOCKER_CERT_PATH}/key.pem"
-docker context use blue
-docker buildx create blue --name blue --driver docker-container --use
-docker buildx use blue
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker run --privileged --rm tonistiigi/binfmt --install all
-docker buildx inspect --bootstrap
+#docker context create blue --default-stack-orchestrator=swarm --docker "host=${DOCKER_HOST},ca=${DOCKER_CERT_PATH}/ca.pem,cert=${DOCKER_CERT_PATH}/cert.pem,key=${DOCKER_CERT_PATH}/key.pem"
+#docker context use blue
+#docker buildx create blue --name blue --driver docker-container --use
+#docker buildx use blue
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#docker run --privileged --rm tonistiigi/binfmt --install all
+#docker buildx inspect --bootstrap
 
 export CACHE_DIR="${CACHE_DIR-/tmp/haakco}"
 
@@ -26,7 +26,8 @@ export CACHE_FROM=" --cache-from=type=local,src=${CACHE_DIR}"
 export CACHE_FROM="${CACHE_FROM} --cache-to=type=local,dest=${CACHE_DIR}"
 
 #
-CMD='docker buildx build --load '"${CACHE_FROM}"' --platform  linux/amd64 --build-arg BASE_UBUNTU_VERSION='"${BASE_UBUNTU_VERSION}"' --build-arg PHP_VERSION='"${PHP_VERSION}"' --build-arg PROXY='"${PROXY}"' --tag '"${IMAGE_NAME}"' .'
+#CMD='docker buildx build --load '"${CACHE_FROM}"' --platform  linux/amd64,linux/arm64,linux/arm64/v8 --build-arg BASE_UBUNTU_VERSION='"${BASE_UBUNTU_VERSION}"' --build-arg PHP_VERSION='"${PHP_VERSION}"' --build-arg PROXY='"${PROXY}"' --tag '"${IMAGE_NAME}"' .'
+CMD='docker buildx build --push '"${CACHE_FROM}"' --platform  linux/amd64,linux/arm64/v8 --build-arg BASE_UBUNTU_VERSION='"${BASE_UBUNTU_VERSION}"' --build-arg PHP_VERSION='"${PHP_VERSION}"' --build-arg PROXY='"${PROXY}"' --tag '"${IMAGE_NAME}"' .'
 #CMD='docker buildx build --push --platform  linux/arm64,linux/amd64 --build-arg BASE_UBUNTU_VERSION='"${BASE_UBUNTU_VERSION}"' --build-arg PHP_VERSION='"${PHP_VERSION}"' --build-arg PROXY='"${PROXY}"' --tag '"${IMAGE_NAME}"' .'
 #CMD='docker buildx build --push --platform  linux/amd64 --build-arg BASE_UBUNTU_VERSION='"${BASE_UBUNTU_VERSION}"' --build-arg PHP_VERSION='"${PHP_VERSION}"' --build-arg PROXY='"${PROXY}"' --tag '"${IMAGE_NAME}"' .'
 

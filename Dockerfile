d350a1d420b7b042474bc412054f5247dbb2d65d
Tim Haak <tim@haak.co>
2022-06-08 17:29:21 +0200

    Update ubuntu

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# syntax = docker/dockerfile:1.2
+# syntax = docker/dockerfile:1.3
 ARG BASE_UBUNTU_VERSION='ubuntu:20.04'
 
 FROM ${BASE_UBUNTU_VERSION}
--- build_2004_80_Push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export PROXY=${PROXY:-""}
-export BASE_UBUNTU_VERSION='ubuntu:20.04'
-export PHP_VERSION='8.0'
-export IMAGE_NAME=haakco/ubuntu2004-php80
-./baseBuild.sh
-docker push "${IMAGE_NAME}"

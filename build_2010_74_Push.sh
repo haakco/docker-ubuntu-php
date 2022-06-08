4de9df252a7cfe70d61bed27f8f13057ac0348f9
Tim Haak <tim@haak.co>
2022-06-08 15:09:35 +0200

    Try fix rotate

--- build_2010_74_Push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export PROXY=${PROXY:-""}
-export BASE_UBUNTU_VERSION='ubuntu:20.10'
-export PHP_VERSION='7.4'
-export IMAGE_NAME=haakco/ubuntu2010-php74
-./baseBuild.sh
-docker push "${IMAGE_NAME}"
